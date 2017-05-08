0d6c2025cbc5ba7ae4b40e6614a490950e98c73c
cluborega <cluborega@gmail.com>
2017-05-08 12:17:29 -0700

    connectionmanager.java dblookup string

--- src/TritonWebReg/dao/ConnectionManager.java
+++ src/TritonWebReg/dao/ConnectionManager.java
@@ -12,13 +12,30 @@ public class ConnectionManager {
     private static final String INIT_LOOOKUP = "java:comp/env";
     private static final String DB_LOOKUP = "jdbc/sqlserver";
 
-    public static Connection conn = null;
+    public static Connection con = null;
 
     /**
      * Creates a new ConnectionManager.  Getting an actual one should always be
      * used with getConnection()
      */
     public ConnectionManager(){
+    }
+
+    /**
+     * Creates and returns a new Connection for a user
+     * @return - a connection to the database
+     */
+    public static Connection getConnection() {
+        try {
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup(INIT_LOOOKUP);
+            DataSource ds = (DataSource) envContext.lookup(DB_LOOKUP);
+            con = ds.getConnection();
+            con.setAutoCommit(false);           // use transactions
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return con;
     }
 
     /**
